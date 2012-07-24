79afefa5840554e12b61d6c4ad1fc36dbf79c208
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-07-24 17:05:35 +0900

    Initial Commit

---
+++ RestKitSample-ObjC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RestKitSample-ObjC
+//
+//  Created by 聡 海老沢 on 12/07/24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
